6dc410ca14f1f238e1499755d1dd75c9e4d70b82
Jordan <mcgalliard.jordan@gmail.com>
2021-12-08 18:11:38 +0300

    Finish js logic. Small HTML & CSS changes.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/* ***********
-Game Logic 
-***************/
-
 /* To do:
 Find alternative to innerHTML
 Make document prettier - alignment of buttons - Better pictures
@@ -33,61 +29,27 @@ function playRound(playerSelection, computerSelection) {
     else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
     (playerSelection === 'paper' && computerSelection === 'rock') ||
     (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        rounds_won += 1;
         return "Player Wins!";
     }
 
     else {
+        rounds_lost += 1;
         return "Computer Wins!";
     }
 
 }
 
-/* Returns true if valid else if false */
-function validateSelection(playerSelection) {
-    
-    playerSelection = playerSelection.toLowerCase()
-
-    if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
-        return false;
+function gameResult(rounds_lost, rounds_won) {
+    if (rounds_won === rounds_lost) {
+        return 'Game tied!';
     }
-
-    else {
-        return true;
-    }
-
-}
-
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    for (let gameNum = 1; gameNum <= 5; gameNum++) {
-
-        playerSelection = prompt("Please enter your hand: ");
-        computerSelection = computerPlay();
-        gameResult = playRound(playerSelection, computerSelection);
-
-        if (gameResult === "Player Wins!") {
-            playerWins++;
-        }
-        else if (gameResult === "Computer Wins!") {
-            computerWins++;   
-        }
-
-        console.log(gameResult);
-        
-    }
-
-    if (playerWins > computerWins) {
-        console.log("You win the game!");
-    }
-    else if (playerWins < computerWins) {
-        console.log("You lost the game :(");
+    else if (rounds_won > rounds_lost) {
+        return 'You won the game!';
     }
     else {
-        console.log("Game was tied!");
+        return 'You lost the game!';
     }
-
-
 }
 
 /* Get elements */
@@ -95,11 +57,14 @@ startButton = document.querySelector("#start-button");
 rockButton = document.querySelector(".player-box .rock-box button");
 paperButton = document.querySelector(".player-box .paper-box button");
 scissorsButton = document.querySelector(".player-box .scissor-box button");
+playerImage = document.querySelector(".player-choice-box img");
+aiImage = document.querySelector(".ai-choice-box img");
+
 
 /* Add event listeners */
 rockButton.addEventListener('click', () => playGame('rock'));
 paperButton.addEventListener('click', () => playGame('paper'));
-scissorsButton.addEventListener('click', () => playGame('scissor'));
+scissorsButton.addEventListener('click', () => playGame('scissors'));
 
 /* Global Variables */
 let gameState = 'not started';
@@ -111,20 +76,66 @@ let rounds_lost = 0;
 
 startButton.addEventListener('click', function() {
     if (gameState === 'not started') {
+        removeScales();
         gameState = 'playing';
         this.innerHTML = "Please pick rock, paper, or scissors.";
+        document.querySelector(".game-display").innerHTML = 'Pick a weapon!'
+        document.querySelector(".player-score-box").innerHTML = '0';
+        document.querySelector(".ai-score-box").innerHTML = '0';
+
     }
 })
 
-
-
+// Remove scale from previous human and AI choices
+function removeScales() {
+        
+    scaledBoxes = Array.from(document.getElementsByClassName('scaled'));
+    scaledBoxes.forEach(function (element) {
+        element.classList.remove('scaled');
+        })
+}
 
 
 function playGame(weapon) {
-    console.log(weapon);
+
     if (gameState === 'playing') {
+
+        // Remove scale from previous human and AI choices
+        removeScales();
+
+        // Get the computer's move
+        computerMove = computerPlay();
+
+        // Get the elements that will be enlarged to show the ai and human choice. Enlarge them.
+        aiBox = document.querySelector(`div[data-weapon='${computerMove}']`);
+        aiBox.classList.add('scaled');
+        humanBox = document.querySelector(`div[data-humanWeapon='${weapon}']`);
+        humanBox.classList.add('scaled');
+        
+        // Game & Round logic
         round += 1;
-        computer_move = computerPlay();
-        console.log(playRound(weapon, computer_move));
+        roundResult = playRound(weapon, computerMove);
+
+        document.querySelector(".player-score-box").innerHTML = rounds_won.toString();
+        document.querySelector(".ai-score-box").innerHTML = rounds_lost.toString();
+
+        if (round == 5) {
+            // Reset play button
+            startButton.innerHTML = "PLAY AGAIN";
+            
+            // Logic to display winner
+            document.querySelector(".game-display").innerHTML = `Round ${round}: ${roundResult} \n ${gameResult(rounds_lost, rounds_won)}`;
+
+            // Reset Game State
+            round = 0;
+            rounds_lost = 0;
+            rounds_won = 0;
+            gameState = 'not started';
+
+            return;
+        }
+
+        document.querySelector(".game-display").innerHTML = `Round ${round}: ${roundResult}`;
+
     }
 };
